2e8385d702a9acedb393492a8978a700743ea7e3
AmoghThakurdesai <amogh.thakurdesai@gmail.com>
2023-09-11 23:19:33 +0530

    Pong game works, game doesnt get over

--- static/pong.js
+++ static/pong.js
@@ -1,7 +1,19 @@
 const ball = document.getElementById('ball');
 const player1 = document.getElementById('box1');
 const player2 = document.getElementById('box2');
-const container = document.getElementsByClassName('ponggame')
+const container = document.getElementById('ponggame');
+const gameover = document.getElementById('gameoverdiv');
+
+function checkCollision(div1, div2) {
+    var rect1 = div1.getBoundingClientRect();
+    var rect2 = div2.getBoundingClientRect();
+
+    return !(rect1.bottom < rect2.top 
+            || rect1.top > rect2.bottom
+            || rect1.right < rect2.right
+            || rect1.left > rect2.right)
+}
+
 // Set initial position
 var posBallX = ball.offsetLeft;
 var posBallY = ball.offsetTop;
@@ -26,14 +38,20 @@ setInterval(
         posBallY = posBallY + speedY;
         console.log({"posBallX":posBallX,"posBallY":posBallY})
         console.log({"maxYplayer":maxYplayer,"maxY":maxY,"maxX":maxX,"minX":minX,"minY":minY })
+        console.log({"containerwidth":container.clientWidth,"containerheight":container.clientHeight})
 
-        if (posBallX < minX) {
+        if (posBallX < minX && checkCollision(ball,player1)) {
             posBallX = minX;
             speedX *= -1; // Change direction
-        } else if (posBallX > maxX) {
+        } else if (posBallX > maxX && checkCollision(ball,player2)) {
             posBallX = maxX;
             speedX *= -1; // Change direction
-        }
+        } 
+        // else {
+        //     var parentDiv = container.parentNode;
+        //     console.log({"parentDiv":parentDiv})
+        //     parentDiv.replaceChild(container, gameover)
+        // }
     
         if (posBallY < minY) {
             posBallY = minY;
@@ -46,7 +64,7 @@ setInterval(
         ball.style.top = posBallY + 'px';
         ball.style.left = posBallX + 'px';
     },
-    200
+    20
 ) // update pos every 20ms
 
 // Listen for keydown event
@@ -57,22 +75,22 @@ window.addEventListener('keydown', function(e) {
     switch (e.key) {     
         case "w":
             {
-                p1top = (p1top - 5);
+                p1top = (p1top - 15);
             }
             break;
         case "s":
             {
-                p1top = p1top + 5;
+                p1top = p1top + 15;
             }
             break;    
         case "ArrowUp":
             {    
-                p2top = p2top - 5;
+                p2top = p2top - 15;
             }
             break;
         case "ArrowDown":
             {
-                p2top = (p2top + 5);
+                p2top = (p2top + 15);
             }
             break;
     }
